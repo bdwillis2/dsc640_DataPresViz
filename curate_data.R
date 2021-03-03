83b3b81f2fa11d3c28f4183bcbe14ee816529979
bdwillis2 <neo154@ARIES.localdomain>
2021-03-03 16:49:58 -0600

    Updated to have Task3 documentation and data

--- curate_data.R
+++ curate_data.R
@@ -1,8 +1,40 @@
-# VERSION: 1
+# VERSION: 3
 library(dplyr)
 library(readxl)
 library(reshape2)
 options('width'=270)
+
+# Quicvk function to read and  have smiilar transforms to normal set
+apd_fatality_csv = function(file_name){
+  # Reading base file
+  base_df = read.csv(file_name, na.strings="n/a")
+  # Using filename instead of date field in data since they switch representations/format many times
+  base_df$year = strsplit(basename(file_name),"_")[[1]][1]
+  base_df$transportation_type = "motor_vehicles"
+  base_df = base_df %>% mutate(month=case_when(
+    Month == "Jan" ~ "January",
+    Month == "Feb" ~ "February",
+    Month == "Mar" ~ "March",
+    Month == "Apr" ~ "April",
+    Month == "May" ~ "May",
+    Month == "Jun" ~ "June",
+    Month == "Jul" ~ "July",
+    Month == "Aug" ~ "August",
+    Month == "Sep" ~ "September",
+    Month == "Oct" ~ "October",
+    Month == "Nov" ~ "November",
+    Month == "Dec" ~ "December",
+    Month == "DEC" ~ "December",
+  ))
+  # Making copies to transform a bit
+  incidents_df = base_df
+  fatalities_df = base_df
+  incidents_df$incident_type = "fatal_accidents"
+  fatalities_df$incident_type = "fatalities"
+  incidents_df$value = 1
+  fatalities_df$value = fatalities_df$"Number.of.Fatalities"
+  return(bind_rows(incidents_df,fatalities_df))
+}
 
 # Could be done using dyplr commands in many instances but did this incrementally
 # to keep it legible and because that is how I was looking at the data at that time
@@ -11,9 +43,17 @@ options('width'=270)
 airline_safety = read_excel("raw_data/airline-safety.csv")
 
 # Here are more supporting data
-crash_fatality_2012 = read_excel("raw_data/2012-apd-crash-fatality-data-1.csv") # Austin texas data https://data.austintexas.gov/w/ergh-7g8p/7r79-5ncn?cur=rK4gDgqgsaG
+base_crash_fatality_2012 = read_excel("raw_data/2012-apd-crash-fatality-data-1.csv") # Austin texas data https://data.austintexas.gov/w/ergh-7g8p/7r79-5ncn?cur=rK4gDgqgsaG
 crash_report = read_excel("raw_data/CrashReport.csv", sheet="crashes_by_year") # National Highway Traffic Safety Administration (NHTSA) Motor Vehicle Crash Data Querying and Reporting
 monroe_county_crash = read_excel("raw_data/monroe-county-crash-data2003-to-2015.csv") # Crash data for single county in Indiana (https://catalog.data.gov/dataset/traffic-data/resource/e46a5cc5-ed4d-4b8d-b750-18e6c9ec570e)
+# All of these are apd from data.gov
+apd_fatality_2013 = apd_fatality_csv("raw_data/2013_APD_Traffic_Fatalities.csv")
+apd_fatality_2014 = apd_fatality_csv("raw_data/2014_APD_Traffic_Fatalities.csv")
+apd_fatality_2015 = apd_fatality_csv("raw_data/2015_APD_Traffic_Fatalities.csv")
+apd_fatality_2016 = apd_fatality_csv("raw_data/2016_APD_Traffic_Fatalities.csv")
+apd_fatality_2017 = apd_fatality_csv("raw_data/2017_APD_Traffic_Fatalities.csv")
+apd_fatality_2018 = apd_fatality_csv("raw_data/2018_APD_Traffic_Fatality_Data_021219.csv")
+apd_fatality_2019 = apd_fatality_csv("raw_data/2019_APD_Traffic_Fatality_Data.csv")
 
 # Now to transform the data and start mixing and mangling them
 airline_safety = melt(airline_safety, id.vars=c("airline", "avail_seat_km_per_week"),
@@ -28,8 +68,15 @@ airline_safety$year = airline_safety$end_year
 airline_safety$transportation_type = "airline"
 
 # Transforming supporting data sets
-crash_fatality_2012$year = format(crash_fatality_2012$Date, format="%Y")
-crash_fatality_2012$transportation_type = "motor_vehicles"
+base_crash_fatality_2012$year = format(base_crash_fatality_2012$Date, format="%Y")
+base_crash_fatality_2012$Date = as.character(base_crash_fatality_2012$Date)
+base_crash_fatality_2012$transportation_type = "motor_vehicles"
+incidents_crash_fatality_2012 = base_crash_fatality_2012
+fatalities_crash_fatality_2012 = base_crash_fatality_2012
+incidents_crash_fatality_2012$incident_type = "fatal_accidents"
+incidents_crash_fatality_2012$value = 1
+fatalities_crash_fatality_2012$incident_type = "fatalities"
+fatalities_crash_fatality_2012$value = 1
 crash_report = melt(crash_report, id.vars="Year")
 crash_report$year = as.character(crash_report$Year)
 crash_report$transportation_type = "motor_vehicles"
@@ -47,15 +94,23 @@ monroe_county_crash = monroe_county_crash %>% mutate(month=case_when(
   Month == 10 ~ "October",
   Month == 11 ~ "November",
   Month == 12 ~ "December",
-), Day=as.character(Day))
+), Day=as.character(Day), Month=as.character(Month))
 monroe_county_crash$transportation_type = "motor_vehicles"
 
 # Now start mangling it together
 finalized_structure = bind_rows(
   airline_safety,
   crash_report,
-  crash_fatality_2012,
-  monroe_county_crash
+  monroe_county_crash,
+  incidents_crash_fatality_2012,
+  fatalities_crash_fatality_2012,
+  apd_fatality_2013,
+  apd_fatality_2014,
+  apd_fatality_2015,
+  apd_fatality_2016,
+  apd_fatality_2017,
+  apd_fatality_2018,
+  apd_fatality_2019
 )
 
-write.csv(file="visualization_data.csv", finalized_structure, row.names=FALSE)
+write.csv(file="visualization_data_task3.csv", finalized_structure, row.names=FALSE)
